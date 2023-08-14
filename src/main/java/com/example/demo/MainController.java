c37c3fc5fdbe523245f2ad56cdc86092c86b7690
bumkee <bert9403202@naver.com>
2023-08-14 11:42:36 +0900

    add health

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -19,5 +19,13 @@ public class MainController {
 		
 		return ResponseEntity.ok().body(map);
 	}
+	
+	@GetMapping({"","/"})
+	public ResponseEntity<?> health(){
+		Map<String, Object> map = new HashMap<>();
+		map.put("key", "2222");
+		
+		return ResponseEntity.ok().body(map);
+	}
 
 }
